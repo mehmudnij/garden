bc12a83cf0abf5dfb94e54b6a26609b5d4ea0fc8
mehmudnij <mehmudnijjad+1@gmail.com>
2025-03-24 13:47:29 +0400

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -17,3 +17,24 @@ nixpkgs.mkShell {
     nixpkgs.kubernetes-helm
   ];
 }
+
+
+{
+  nixpkgs ? import <nixpkgs> { },
+  toolVersions ? import ./nix/tool-versions.nix,
+  nodejs ? nixpkgs.${"nodejs_" + toString toolVersions.nodejs},
+}:
+nixpkgs.mkShell {
+  env = {
+    GARDEN_DISABLE_ANALYTICS = true;
+    GARDEN_DISABLE_VERSION_CHECK = true;
+    ANALYTICS_DEV = true;
+  };
+
+  packages = [
+    nodejs
+    nixpkgs.jq
+    nixpkgs.kubectl
+    nixpkgs.kubernetes-helm
+  ];
+}
